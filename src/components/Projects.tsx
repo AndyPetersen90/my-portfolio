95826f93910f837cd69bacd35655d857f18d093d
AndyPetersen90 <andrew.petersen.webdev@gmail.com>
2025-06-11 14:44:27 -0600

    Adding ASL Forms to project list

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,20 +4,119 @@ function ProjectsSection() {
       <h2 className="text-center text-3xl font-bold mb-6 text-black">Past Projects</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[2rem]">
         <div className="border border-[#ccc] rounded-lg p-[2rem] bg-[#f9f9f9] hover:bg-[#e0e0e0] hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out">
-          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">KPNailGeek Website Demo</h3>
-          <a href="https://www.youtube.com/watch?v=L8YZ12ITwsc" target="_blank">
+          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">ASL Quote Requst</h3>
+          <a href="https://word-fishing.netlify.app/" target="_blank">
             <img
               className="w-full h-[200px] object-cover rounded-lg mb-[1rem]"
-              src="./kpnailgeek-demo-screenshot.png"
-              alt="KPNailGeek Website Demo Screenshot"
+              src="./asl-quote-screenshot.png"
+              alt="All Season Lighting Static Page"
             />
           </a>
           <p className="text-[1rem] text-[#333] text-center mb-[.5rem] underline">
-            Tech Used: HTML | CSS | Javascript | Node.js | Axios | Express
+            Tech Used: React | Typescript | Vite | Tailwind | Javascript | OAuth | Prisma | Node.js | AWS | Vercel
           </p>
           <p className="text-[1em] text-[#333] text-center mb-[1rem]">
-            This was my first ever website. It was my mid bootcamp project. It was designed to be a simple website displaying my wifes nail tech
-            business, pricing, and portfolio work.
+            ASLForms - A Full-Stack Web Application A modern web application built with React, TypeScript, and Express that helps manage and process
+            ASL-related forms. Features include secure file uploads via AWS S3, type-safe database operations with Prisma, and a responsive UI built
+            with TailwindCSS. The application demonstrates full-stack development capabilities, modern web architecture, and cloud integration.
+          </p>
+        </div>
+
+        <div className="border border-[#ccc] rounded-lg p-[2rem] bg-[#f9f9f9] hover:bg-[#e0e0e0] hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out">
+          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">Word Fishing Game</h3>
+          <a href="https://word-fishing.netlify.app/" target="_blank">
+            <img
+              className="w-full h-[200px] object-cover rounded-lg mb-[1rem]"
+              src="./word-fishing-game-image.png"
+              alt="All Season Lighting Static Page"
+            />
+          </a>
+          <p className="text-[1rem] text-[#333] text-center mb-[.5rem] underline">Tech Used: React | Typescript | Tailwind | Javascript</p>
+          <p className="text-[1em] text-[#333] text-center mb-[1rem]">
+            In my church, I help lead the children in singing religious music. I built this quick little word fishing game. Its a simple game that is
+            based on Hangman, but more kid friendly. When a letter is guessed and it is part of the word, the letter is shown in the word as well as
+            the fist moves closer to the fisherman. This was a prett simple project, but it was fun to be able to work with animations and CSS to make
+            it look nice.
+          </p>
+        </div>
+
+        <div className="border border-[#ccc] rounded-lg p-[2rem] bg-[#f9f9f9] hover:bg-[#e0e0e0] hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out">
+          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">Word Removal Game</h3>
+          <a href="https://word-removal-game.netlify.app/" target="_blank">
+            <img
+              className="w-full h-[200px] object-cover rounded-lg mb-[1rem]"
+              src="./word-remover-game-screenshot.png"
+              alt="All Season Lighting Static Page"
+            />
+          </a>
+          <p className="text-[1rem] text-[#333] text-center mb-[.5rem] underline">Tech Used: React | Typescript | Tailwind | Javascript</p>
+          <p className="text-[1em] text-[#333] text-center mb-[1rem]">
+            In my church, I help lead the children in singing religious music. I built this quick little website to allow me to easily test the kids
+            memory by progressively removing the words to any song they are learning. This was a fun and quick project to build. I was able to play
+            with custom fond sizing and global themes with Tailwind.
+          </p>
+        </div>
+
+        <div className="border border-[#ccc] rounded-lg p-[2rem] bg-[#f9f9f9] hover:bg-[#e0e0e0] hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out">
+          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">All Season Lighting</h3>
+          <a href="https://www.allseasonlighting.com/" target="_blank">
+            <img
+              className="w-full h-[200px] object-cover rounded-lg mb-[1rem]"
+              src="./all-season-lighting-screenshot.png"
+              alt="All Season Lighting Static Page"
+            />
+          </a>
+          <p className="text-[1rem] text-[#333] text-center mb-[.5rem] underline">Tech Used: Astro | Tailwind | Typescript | Lambda | Vercel</p>
+          <p className="text-[1em] text-[#333] text-center mb-[1rem]">
+            Helped build a static page for a former coworker. This is static page for a company that offers perminent lighting solutions for homes.
+            This is designed with a few images of past work as well as a option for people to request quotes or request to become a seller. Both of
+            these forms use an AWS Lamda function to send the requests to the company's email. This project was fun as I got to use Astro and practice
+            building a static page with serverside and clientside rendering.
+          </p>
+        </div>
+
+        <div className="border border-[#ccc] rounded-lg p-[2rem] bg-[#f9f9f9] hover:bg-[#e0e0e0] hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out">
+          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">Past Work at Sunrebel</h3>
+          <a href="https://www.youtube.com/playlist?list=PLlFtToxLwJR73Ewb-pY2kPb0RQsUpGNB3" target="_blank">
+            <img
+              className="w-full h-[200px] object-cover rounded-lg mb-[1rem]"
+              src="./sunrebel-proposals-image.png"
+              alt="Sunrebel Company Static Page"
+            />
+          </a>
+          <p className="text-[1rem] text-[#333] text-center mb-[.5rem] underline">
+            Tech Used: React | Typescript | MUI | Zustand | Tanstack Query | Express | Node.js | MongoDB | AWS
+          </p>
+          <p className="text-[1em] text-[#333] text-center mb-[1rem]">
+            This is a link to a youtube playlist of some short screen recordings I made of some of the projects I completed while working at Sunrebel.
+            <br />
+            There are many more, but due to the nature of the code, I am not able to record those.
+          </p>
+        </div>
+
+        <div className="border border-[#ccc] rounded-lg p-[2rem] bg-[#f9f9f9] hover:bg-[#e0e0e0] hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out">
+          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">Exercises Web App</h3>
+          <a href="https://react-exercises-web-app.netlify.app/" target="_blank">
+            <img className="w-full h-[200px] object-cover rounded-lg mb-[1rem]" src="./exercises-web-app.png" alt="Exercises Web App Screenshot" />
+          </a>
+          <p className="text-[1rem] text-[#333] text-center mb-[.5rem] underline">Tech Used: React | MUI | RapidAPI</p>
+          <p className="text-[1em] text-[#333] text-center mb-[1rem]">
+            This was another practice website. I built this in react as well as using a third party API to get the exercise data.
+          </p>
+        </div>
+
+        <div className="border border-[#ccc] rounded-lg p-[2rem] bg-[#f9f9f9] hover:bg-[#e0e0e0] hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out">
+          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">Random Quote Generator</h3>
+          <a href="https://andrews-react-quote-generator.netlify.app/" target="_blank">
+            <img
+              className="w-full h-[200px] object-cover rounded-lg mb-[1rem]"
+              src="./Screen Shot 2022-06-29 at 4.53.33 PM.png"
+              alt="Random Quote Generator Screenshot"
+            />
+          </a>
+          <p className="text-[1rem] text-[#333] text-center mb-[.5rem] underline">Tech Used: React | CSS | Axios | RapidAPI</p>
+          <p className="text-[1em] text-[#333] text-center mb-[1rem]">
+            This was a simple website to generate random quotes. I used this project to practice using React and API calls.
           </p>
         </div>
 
@@ -42,125 +141,35 @@ function ProjectsSection() {
         </div>
 
         <div className="border border-[#ccc] rounded-lg p-[2rem] bg-[#f9f9f9] hover:bg-[#e0e0e0] hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out">
-          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">Random Quote Generator</h3>
-          <a href="https://andrews-react-quote-generator.netlify.app/" target="_blank">
+          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">KPNailGeek Website Demo</h3>
+          <a href="https://www.youtube.com/watch?v=L8YZ12ITwsc" target="_blank">
             <img
               className="w-full h-[200px] object-cover rounded-lg mb-[1rem]"
-              src="./Screen Shot 2022-06-29 at 4.53.33 PM.png"
-              alt="Random Quote Generator Screenshot"
+              src="./kpnailgeek-demo-screenshot.png"
+              alt="KPNailGeek Website Demo Screenshot"
             />
           </a>
-          <p className="text-[1rem] text-[#333] text-center mb-[.5rem] underline">
-            Tech Used: React | CSS | Axios | RapidAPI
-          </p>
+          <p className="text-[1rem] text-[#333] text-center mb-[.5rem] underline">Tech Used: HTML | CSS | Javascript | Node.js | Axios | Express</p>
           <p className="text-[1em] text-[#333] text-center mb-[1rem]">
-            This was a simple website to generate random quotes. I used this project to practice using React and API calls.
-          </p>
-        </div>
-
-        <div className="border border-[#ccc] rounded-lg p-[2rem] bg-[#f9f9f9] hover:bg-[#e0e0e0] hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out">
-          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">Exercises Web App</h3>
-          <a href="https://react-exercises-web-app.netlify.app/" target="_blank">
-            <img className="w-full h-[200px] object-cover rounded-lg mb-[1rem]" src="./exercises-web-app.png" alt="Exercises Web App Screenshot" />
-          </a>
-          <p className="text-[1rem] text-[#333] text-center mb-[.5rem] underline">
-            Tech Used: React | MUI | RapidAPI
-          </p>
-          <p className="text-[1em] text-[#333] text-center mb-[1rem]">
-            This was another practice website. I built this in react as well as using a third party API to get the exercise data.
+            This was my first ever website. It was my mid bootcamp project. It was designed to be a simple website displaying my wifes nail tech
+            business, pricing, and portfolio work.
           </p>
         </div>
 
         {/* <div className="border border-[#ccc] rounded-lg p-[2rem] bg-[#f9f9f9] hover:bg-[#e0e0e0] hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out">
-          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">Sunrebel Info Page (Previous Role)</h3>
-          <a href="https://www.sunrebel.com/" target="_blank">
-            <img
-              className="w-full h-[200px] object-cover rounded-lg mb-[1rem]"
-              src="./sunrebel-proposals-image.png"
-              alt="Sunrebel Company Static Page"
-            />
-          </a>
-          <p className="text-[1em] text-[#333] text-center mb-[1rem]">
-            This is the main landing page for the company I previously worked at. This will go over what the web app was designed to do and how it was
-            used.
-          </p>
-        </div> */}
-
-        <div className="border border-[#ccc] rounded-lg p-[2rem] bg-[#f9f9f9] hover:bg-[#e0e0e0] hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out">
-          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">Past Work at Sunrebel</h3>
-          <a href="https://www.youtube.com/playlist?list=PLlFtToxLwJR73Ewb-pY2kPb0RQsUpGNB3" target="_blank">
-            <img
-              className="w-full h-[200px] object-cover rounded-lg mb-[1rem]"
-              src="./sunrebel-proposals-image.png"
-              alt="Sunrebel Company Static Page"
-            />
-          </a>
-          <p className="text-[1rem] text-[#333] text-center mb-[.5rem] underline">
-            Tech Used: React | Typescript | MUI | Zustand | Tanstack Query | Express | Node.js | MongoDB | AWS
-          </p>
-          <p className="text-[1em] text-[#333] text-center mb-[1rem]">
-            This is a link to a youtube playlist of some short screen recordings I made of some of the projects I completed while working at Sunrebel.
-            <br />
-            There are many more, but due to the nature of the code, I am not able to record those.
-          </p>
-        </div>
-
-        <div className="border border-[#ccc] rounded-lg p-[2rem] bg-[#f9f9f9] hover:bg-[#e0e0e0] hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out">
-          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">All Season Lighting</h3>
-          <a href="https://www.allseasonlighting.com/" target="_blank">
-            <img
-              className="w-full h-[200px] object-cover rounded-lg mb-[1rem]"
-              src="./all-season-lighting-screenshot.png"
-              alt="All Season Lighting Static Page"
-            />
-          </a>
-          <p className="text-[1rem] text-[#333] text-center mb-[.5rem] underline">
-            Tech Used: Astro | Tailwind | Typescript | Lambda | Vercel
-          </p>
-          <p className="text-[1em] text-[#333] text-center mb-[1rem]">
-            Helped build a static page for a former coworker. This is static page for a company that offers perminent lighting solutions for homes.
-            This is designed with a few images of past work as well as a option for people to request quotes or request to become a seller. Both of these forms use an AWS Lamda function to send the requests to the company's email. This project was fun as I got to
-            use Astro and practice building a static page with serverside and clientside rendering.
-          </p>
-        </div>
-
-        <div className="border border-[#ccc] rounded-lg p-[2rem] bg-[#f9f9f9] hover:bg-[#e0e0e0] hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out">
-          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">Word Removal Game</h3>
-          <a href="https://word-removal-game.netlify.app/" target="_blank">
-            <img
-              className="w-full h-[200px] object-cover rounded-lg mb-[1rem]"
-              src="./word-remover-game-screenshot.png"
-              alt="All Season Lighting Static Page"
-            />
-          </a>
-          <p className="text-[1rem] text-[#333] text-center mb-[.5rem] underline">
-            Tech Used:  React | Typescript | Tailwind | Javascript
-          </p>
-          <p className="text-[1em] text-[#333] text-center mb-[1rem]">
-            In my church, I help lead the children in singing religious music. I built this quick little website to allow me to easily test the kids
-            memory by progressively removing the words to any song they are learning. This was a fun and quick project to build. I was able to play
-            with custom fond sizing and global themes with Tailwind.
-          </p>
-        </div>
-        <div className="border border-[#ccc] rounded-lg p-[2rem] bg-[#f9f9f9] hover:bg-[#e0e0e0] hover:scale-105 hover:shadow-lg transition duration-300 ease-in-out">
-          <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">Word Fishing Game</h3>
-          <a href="https://word-fishing.netlify.app/" target="_blank">
-            <img
-              className="w-full h-[200px] object-cover rounded-lg mb-[1rem]"
-              src="./word-fishing-game-image.png"
-              alt="All Season Lighting Static Page"
-            />
-          </a>
-          <p className="text-[1rem] text-[#333] text-center mb-[.5rem] underline">
-            Tech Used: React | Typescript | Tailwind | Javascript
-          </p>
-          <p className="text-[1em] text-[#333] text-center mb-[1rem]">
-            In my church, I help lead the children in singing religious music. I built this quick little word fishing game. Its a simple game that is
-            based on Hangman, but more kid friendly. When a letter is guessed and it is part of the word, the letter is shown in the word as well as
-            the fist moves closer to the fisherman. This was a prett simple project, but it was fun to be able to work with animations and CSS to make
-            it look nice.
-          </p>
-        </div>
+                <h3 className="text-[1.5em] text-[#333] text-center mb-[1rem]">Sunrebel Info Page (Previous Role)</h3>
+                <a href="https://www.sunrebel.com/" target="_blank">
+                  <img
+                    className="w-full h-[200px] object-cover rounded-lg mb-[1rem]"
+                    src="./sunrebel-proposals-image.png"
+                    alt="Sunrebel Company Static Page"
+                  />
+                </a>
+                <p className="text-[1em] text-[#333] text-center mb-[1rem]">
+                  This is the main landing page for the company I previously worked at. This will go over what the web app was designed to do and how it was
+                  used.
+                </p>
+              </div> */}
       </div>
     </section>
   );
